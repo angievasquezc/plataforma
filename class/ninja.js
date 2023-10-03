8ed32174a7710e52bb7c8b9c7029491ed83acbf9
Angela Vasquez <amvasquez1993@gmail.com>
2023-10-03 14:02:04 -0500

    [Tarea] Sensei

--- class/ninja.js
+++ class/ninja.js
@@ -1,17 +1,17 @@
 class Ninja{
-    constructor(nombreAtr, saludAtr, velocidadAtr, fuerzaAtr){
+    constructor(nombreAtr, saludAtr, velocidadAtr=3, fuerzaAtr=3){
         this.nombre=nombreAtr;
         this.salud=saludAtr;
-        this.velocidad=3;
-        this.fuerza=3;
+        this.velocidad=velocidadAtr;
+        this.fuerza=fuerzaAtr;
     }
     sayName(){
         console.log(`Mi nombre es ${this.nombre}`);
     }
     showStats(){
-        console.log(`nombre: ${this.nombre} salud: ${this.salud} velocidad ${this.velocidad} fuerza ${this.fuerza}`)
+        console.log(`nombre: ${this.nombre} salud: ${this.salud} velocidad: ${this.velocidad} fuerza: ${this.fuerza}`)
     }
-    drinkSaje(){
+    drinkSake(){
         console.log(this.salud+=10);
     }
 }
@@ -20,7 +20,37 @@ class Ninja{
 const ninja1=new Ninja("Ninja1", 10);
 ninja1.sayName();
 ninja1.showStats();
-ninja1.drinkSaje();
+ninja1.drinkSake();
+
+class Sensei extends Ninja{
+    constructor(nombreAtr, saludAtr=200, velocidadAtr=10, fuerzaAtr=10, sabiduriaAtr=10){
+        super(nombreAtr, saludAtr, velocidadAtr, fuerzaAtr);
+        this.sabiduria=sabiduriaAtr;
+    }
+ 
+    speakWisdom(){
+        super.drinkSake()
+        return`mensaje de sabiduría ` ;
+             
+       
+        
+    }
+    showStats(){
+        // const message = super.showStats();
+        // console.log(message);
+    //    super.showStats();
+        return `nombre:${this.nombre} salud: ${this.salud} velocidad: ${this.velocidad} fuerza: ${this.fuerza}(;) sabiduría: ${this.sabiduria}`
+    }
+    }
+
+
+const sensei1= new Sensei("sensei1", 200, 10, 10);
+console.log("resultados sensei");
+console.log(sensei1.speakWisdom());
+console.log(sensei1.showStats());
+
+
+
 
 
 
